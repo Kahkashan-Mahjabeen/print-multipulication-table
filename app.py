c4f91ad9fa6c89785014ad00c1f09e9f67462dd1
Kahkashan-Mahjabeen <mahiali9903@gmail.com>
2021-07-18 19:57:46 +0530

    calculator added

--- app.py
+++ app.py
@@ -11,9 +11,16 @@
 
 from flask import Flask, render_template
 app = Flask(__name__)
+app.debug = True
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/calculator')
+def home():
+    return render_template('calculator.html')
+
+
 
 if __name__ == '__main__':
     app.run()
